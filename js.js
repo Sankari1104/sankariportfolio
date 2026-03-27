563b4d5dbbe2202bea0227c736342f9fe8b57221
Sankarivinayagam <sankarivinayagam1107@gmail.com>
2026-03-27 16:02:42 +0530

    Create js.js

---
+++ js.js
@@ -0,0 +1,7 @@
+// Smooth scroll highlight (optional future use)
+console.log("Portfolio Loaded");
+
+// Example: button click alert
+document.querySelector(".btn").addEventListener("click", () => {
+  alert("Contact me for work!");
+});
