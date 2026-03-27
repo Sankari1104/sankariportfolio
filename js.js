9289ef2c118b10721b348fb6b7298ab96d609b41
Sankarivinayagam <sankarivinayagam1107@gmail.com>
2026-03-27 15:52:53 +0530

    Delete js.js

--- js.js
+++
@@ -1,7 +0,0 @@
-// Smooth scroll highlight (optional future use)
-console.log("Portfolio Loaded");
-
-// Example: button click alert
-document.querySelector(".btn").addEventListener("click", () => {
-  alert("Contact me for work!");
-});
